d564c0362e5799918f6c0b585a4eeeae51fa2dc0
MacInLife <marie.ange.coco@gmail.com>
2020-05-27 14:00:13 +0200

    modif reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,6 @@
-import { combineReducers } from 'redux';
-import todo from "./todo"
-
-
-
-/**
- * combineReducers() permet de combiner les différents reducers, ce qui est plus pratique pour organiser les reducers.
- *
- * Dans notre cas le state général de l'application aura cette forme
- * {
- *   listings : {
- *     experiences: [....]
- *   },
- *   app: {
- *     isLoading: true
- *   }
- * }
- */
+import {combineReducers} from 'redux';
+import todo from './todo';
 
 export default combineReducers({
-    todo,
-
-
+  todo,
 });
